d98e4cd29157c70f521db6db955cea6513c138ca
david.fonseca <fonsecasoundcruz@gmail.com>
2023-05-04 12:06:29 -0600

    Cahngee variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production : true,
-  apiURL : 'https://localhost:7222/api/'
+  // apiURL : 'https://localhost:7222/api/' //local
+  apiURL : 'https://localhost:44317/api/' //usuario
 
 };
